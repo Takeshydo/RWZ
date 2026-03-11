f4b5a004d89984463518fad83a8916b5ee83da9c
MathysL <m.lacoque@ecole-ippsi.net>
2026-03-11 16:35:45 +0100

    Camera Move + Patch movement problem

--- Assets/Script/Camera/CameraView.cs
+++ Assets/Script/Camera/CameraView.cs
@@ -1,16 +1,45 @@
 using UnityEngine;
+using UnityEngine.InputSystem;  
 
 public class CameraView : MonoBehaviour
 {
     [Header("Components")]
-    private Transform playerPos;
+    private Transform SpringArm; //Point d'encrage fixe
+    private Transform playerPos; 
+    private InputAction lookActions;
+    
+    [Header("Variables")]
+    private Vector2 moveCam;
+
+    private float offset = 6f;
+    private float sensibility = 75f;
+    private float minPitch = -25f;
+    private float maxPitch = 50f;
+    
+    //Var Rotation Axe Camera
+    private float yaw;
+    private float pitch;
+    
     void Start()
     {
-        playerPos = GetComponentInParent<Transform>();
+        lookActions = InputSystem.actions.FindAction("Look");
+        SpringArm = transform.parent;
+    }
+
+    void Update()
+    {
+        moveCam = lookActions.ReadValue<Vector2>();
+        
+        //Movement sur yaw 
+        yaw += moveCam.x * sensibility *  Time.deltaTime;
+        //Movement sur pitch
+        pitch += moveCam.y * sensibility *  Time.deltaTime;
+        pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
     }
 
     void LateUpdate()
     {
-        
+        transform.eulerAngles = new Vector3(pitch, yaw, 0f);
+        transform.position = SpringArm.position - transform.forward * offset;
     }
 }
--- Assets/Script/Player/Movement.cs
+++ Assets/Script/Player/Movement.cs
@@ -4,12 +4,14 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [Header("Variables")]
-    public float speed = 5f;
+    private float speed = 5f;
+    private float rotateSpeed = 500f;
     private Vector3 moveinput;
     
     
     [Header("Component")]
     private Rigidbody rb;
+    private Camera cam;
 
     [Header("Input")]
     private InputAction move;
@@ -18,6 +20,7 @@ public class Movement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        cam = GetComponentInChildren<Camera>();
         move = InputSystem.actions.FindAction("Move");
     }
     
@@ -25,11 +28,27 @@ public class Movement : MonoBehaviour
     void Update()
     {
         moveinput = new Vector3(move.ReadValue<Vector2>().x, 0f, move.ReadValue<Vector2>().y);
-
+        
+        Rotation();
     }
 
     void FixedUpdate()
     {
         rb.linearVelocity = new Vector3(moveinput.x * speed, rb.linearVelocity.y, moveinput.z * speed);
+    }
+
+
+    void Rotation()
+    {
+        if(moveinput.sqrMagnitude == 0)return;
+        
+        float camY = cam.transform.eulerAngles.y;
+        
+        Vector3 direction = Quaternion.Euler(0, camY, 0) * moveinput;
+        
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+        
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+        
     }
 }
